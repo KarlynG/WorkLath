09965ccd4b2ce6d7a9db0ae27679b868909684a0
jon77717 <Luigi77717@gmail.com>
2021-09-20 20:43:21 -0400

    Chequea la vaina

---
+++ WorkLath/Controllers/JobController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkLath.Bl.Dto;
+using WorkLath.Model.Entities;
+using WorkLath.Service.Services;
+
+namespace WorkLath.Controllers
+{
+    public class JobController : BaseController<Job, JobDto>
+    {
+        public JobController(IJobService service) : base(service)
+        {
+        }
+    }
+}
